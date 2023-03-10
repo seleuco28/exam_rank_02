6f7c2a2f68d66bbe8732e1fc46f55c59ce579a18
Alvaro Velázquez <alvarovelazquez@MacBook-Pro-de-Alvaro.local>
2023-03-10 15:05:44 +0100

    empezando

--- lvl1/first_word.c
+++ lvl1/first_word.c
@@ -1,21 +1,49 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   first_word.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: alvelazq <alvelazq@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/09 13:55:50 by alvelazq          #+#    #+#             */
-/*   Updated: 2023/03/09 13:56:45 by alvelazq         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+/*Assignment name  : first_word
+Expected files   : first_word.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that takes a string and displays its first word, followed by a
+newline.
+
+A word is a section of string delimited by spaces/tabs or by the start/end of
+the string.
+
+If the number of parameters is not 1, or if there are no words, simply display
+a newline.
+
+Examples:
+
+$> ./first_word "FOR PONY" | cat -e
+FOR$
+$> ./first_word "this        ...       is sparta, then again, maybe    not" | cat -e
+this$
+$> ./first_word "   " | cat -e
+$
+$> ./first_word "a" "b" | cat -e
+$
+$> ./first_word "  lorem,ipsum  " | cat -e
+lorem,ipsum$
+$>*/
 
 #include <unistd.h>
 #include <stdio.h>
 
 
-int main()
+int main(int ac, char **av)
 {
-    
+    int i = 0;
+    if (ac == 2)
+    {
+        while (av[1][i] == ' ')
+            i++;
+        while (av[1][i] != ' ' && av[1][i])
+        {
+            write(1, &av[1][i], 1);
+            i++;
+        }
+    }
+    write(1, "\n", 1);
+    return 0;
 
 }
